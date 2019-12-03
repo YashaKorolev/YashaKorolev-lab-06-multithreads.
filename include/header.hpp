b2fd0dc05d0483629c3a1cad4048ecc5276a3cf5
CrazyOverdose <alenkavh@yandex.ru>
2019-12-03 23:45:35 +0300

    test1

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,29 @@
 // Copyright 2018 Your Name <your_email>
+#include "/home/absinthetoxin/CrazyOverdose/lab-06-multithreads/third-party/PicoSHA2/picosha2.h"
+#include <string>
+#include <iostream>
+#include <thread>
+#include <boost/log/trivial.hpp>
 
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
+using namespace std;
 
-#endif // INCLUDE_HEADER_HPP_
+class SHA256{
+
+public:
+    static void getting_a_hash() {
+        for (;;) {
+            string end_hash = "0000";
+            string random = to_string(rand()); //случайные взодные данные
+            string hash_for_random = picosha2::hash256_hex_string(random); //значение хеш-функции
+
+            if (hash_for_random.substr(60) == end_hash) {
+                BOOST_LOG_TRIVIAL(info) << "Suitable SHA256(\"" << random << "\") = \"" << hash_for_random << "\";"
+                                        << endl; //сообщения журнала на консоли
+            }
+            else {
+                BOOST_LOG_TRIVIAL(info) << "Thread ID " << this_thread::get_id() << "  UNSuitable SHA256(\"" << random
+                                        << "\") = \"" << hash_for_random << "\";" << endl;
+            }
+        }
+    }
+};
