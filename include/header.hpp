1d2bb86f16cb3a4c12c9b3437e714f7e9717062c
CrazyOverdose <alenkavh@yandex.ru>
2019-12-06 12:45:26 +0300

    test6

--- include/header.hpp
+++ include/header.hpp
@@ -1,32 +1,34 @@
-// Copyright 2018 Your Name <your_email>
+//Copyright 2019 CrazyOverdose
+
+#ifndef  INCLUDE_HEADER_HPP_
+#define  INCLUDE_HEADER_HPP_
 #include "/home/absinthetoxin/CrazyOverdose/lab-06-multithreads/third-party/PicoSHA2/picosha2.h"
 #include <string>
 #include <iostream>
 #include <thread>
 #include <boost/log/trivial.hpp>
 
-using namespace std;
-
 class SHA256{
 
 public:
     static void getting_a_hash() {
         for (;;) {
-            string end_hash = "0000";
-            string random = to_string(rand()); //случайные взодные данные
+            std::string end_hash = "0000";
+            std::string random = std::to_string(rand()); //случайные взодные данные
             //значение хеш-функции
-            string hash_for_random = picosha2::hash256_hex_string(random);
+            std::string hash_for_random = picosha2::hash256_hex_string(random);
 
             if (hash_for_random.substr(60) == end_hash) {
                 BOOST_LOG_TRIVIAL(info) << "Suitable SHA256(\"" << random
                 << "\") = \"" << hash_for_random << "\";"
-                << endl; //сообщения журнала на консоли
+                << std::endl; //сообщения журнала на консоли
             }
             else {
                 BOOST_LOG_TRIVIAL(info) << "Thread ID "
-                << this_thread::get_id() << "  UNSuitable SHA256(\"" << random
-                << "\") = \"" << hash_for_random << "\";" << endl;
+                << std::this_thread::get_id() << "  UNSuitable SHA256(\"" << random
+                << "\") = \"" << hash_for_random << "\";" << std::endl;
             }
         }
     }
 };
+#endif // INCLUDE_ANALYSIS_HPP_
